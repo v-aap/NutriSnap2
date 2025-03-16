df26e2fdb9b1a80bfd68b0800511827e8820f190
Valeria Arce <145574961+v-aap@users.noreply.github.com>
2025-03-16 14:00:29 -0400

    Update MongoDBManager.swift

--- NutriSnap2/NutriSnap2/Services/MongoDBManager.swift
+++ NutriSnap2/NutriSnap2/Services/MongoDBManager.swift
@@ -37,14 +37,14 @@ class MongoDBManager {
     }
 
     // MARK: - User Login
-    func loginUser(email: String, password: String, completion: @escaping (Bool) -> Void) {
+    func loginUser(email: String, password: String, completion: @escaping (UserModel?) -> Void) {
         let url = URL(string: "\(baseURL)/findOne")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue(apiKey, forHTTPHeaderField: "apiKey")
 
-        let hashedPassword = SecurityManager.shared.hashPassword(password) 
+        let hashedPassword = SecurityManager.shared.hashPassword(password)
 
         let query: [String: Any] = [
             "dataSource": "Cluster0",
@@ -56,10 +56,30 @@ class MongoDBManager {
         request.httpBody = try? JSONSerialization.data(withJSONObject: query)
 
         URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data, let jsonResponse = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                completion(jsonResponse["document"] != nil)
-            } else {
-                completion(false)
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+
+            do {
+                if let jsonResponse = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                   let document = jsonResponse["document"] as? [String: Any] {
+                    
+                    let user = UserModel(
+                        id: document["_id"] as? String ?? UUID().uuidString,
+                        firstName: document["firstName"] as? String ?? "",
+                        lastName: document["lastName"] as? String ?? "",
+                        email: document["email"] as? String ?? "",
+                        password: "", // Password is not stored locally
+                        calorieGoal: document["calorieGoal"] as? Int
+                    )
+                    
+                    completion(user)
+                } else {
+                    completion(nil)
+                }
+            } catch {
+                completion(nil)
             }
         }.resume()
     }
