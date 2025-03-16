6ef0167b7aaa0b938cdabd7040d66c1e5fd6d389
Valeria Arce <145574961+v-aap@users.noreply.github.com>
2025-03-15 20:28:29 -0400

    Update MongoDBManager.swift
    For User Registration/Login only

--- NutriSnap2/NutriSnap2/Services/MongoDBManager.swift
+++ NutriSnap2/NutriSnap2/Services/MongoDBManager.swift
@@ -6,11 +6,10 @@ class MongoDBManager {
     private let baseURL = "https://data.mongodb-api.com/app/67d07e18cbf53618b5f6f80b/endpoint/data/v1/action"
     private let apiKey = "58dc8397-3be1-49d3-8b2c-40bfa2434637"
     private let databaseName = "NutriSnapDB"
-    
-    // MARK: - User Management
     private let usersCollection = "users"
 
-    func registerUser(email: String, password: String, completion: @escaping (Bool) -> Void) {
+    // MARK: - Register User
+    func registerUser(user: UserModel, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(baseURL)/insertOne")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -22,8 +21,11 @@ class MongoDBManager {
             "database": databaseName,
             "collection": usersCollection,
             "document": [
-                "email": email,
-                "password": password // 🚨 Hash passwords in a real app
+                "firstName": user.firstName,
+                "lastName": user.lastName,
+                "email": user.email,
+                "passwordHash": user.passwordHash, 
+                "calorieGoal": user.calorieGoal ?? 2000
             ]
         ]
 
@@ -34,6 +36,7 @@ class MongoDBManager {
         }.resume()
     }
 
+    // MARK: - User Login
     func loginUser(email: String, password: String, completion: @escaping (Bool) -> Void) {
         let url = URL(string: "\(baseURL)/findOne")!
         var request = URLRequest(url: url)
@@ -41,11 +44,13 @@ class MongoDBManager {
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue(apiKey, forHTTPHeaderField: "apiKey")
 
+        let hashedPassword = SecurityManager.shared.hashPassword(password) 
+
         let query: [String: Any] = [
             "dataSource": "Cluster0",
             "database": databaseName,
             "collection": usersCollection,
-            "filter": ["email": email, "password": password]
+            "filter": ["email": email, "passwordHash": hashedPassword] 
         ]
 
         request.httpBody = try? JSONSerialization.data(withJSONObject: query)
@@ -55,88 +60,6 @@ class MongoDBManager {
                 completion(jsonResponse["document"] != nil)
             } else {
                 completion(false)
-            }
-        }.resume()
-    }
-
-    // MARK: - Meals Collection
-    private let mealsCollection = "meals"
-
-    func saveMeal(meal: MealEntry, userEmail: String, completion: @escaping (Bool) -> Void) {
-        let url = URL(string: "\(baseURL)/insertOne")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue(apiKey, forHTTPHeaderField: "apiKey")
-
-        let mealData: [String: Any] = [
-            "dataSource": "Cluster0",
-            "database": databaseName,
-            "collection": mealsCollection,
-            "document": [
-                "userEmail": userEmail,
-                "foodName": meal.foodName,
-                "date": ISO8601DateFormatter().string(from: meal.date),
-                "calories": meal.calories,
-                "carbs": meal.carbs,
-                "protein": meal.protein,
-                "fats": meal.fats,
-                "mealType": meal.mealType
-            ]
-        ]
-
-        request.httpBody = try? JSONSerialization.data(withJSONObject: mealData)
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            completion(error == nil)
-        }.resume()
-    }
-
-    func fetchMeals(userEmail: String, completion: @escaping ([MealEntry]?) -> Void) {
-        let url = URL(string: "\(baseURL)/find")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue(apiKey, forHTTPHeaderField: "apiKey")
-
-        let query: [String: Any] = [
-            "dataSource": "Cluster0",
-            "database": databaseName,
-            "collection": mealsCollection,
-            "filter": ["userEmail": userEmail]
-        ]
-
-        request.httpBody = try? JSONSerialization.data(withJSONObject: query)
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                completion(nil)
-                return
-            }
-
-            do {
-                if let jsonResponse = try JSONSerialization.jsonObject(with: data) as? [String: Any],
-                   let documents = jsonResponse["documents"] as? [[String: Any]] {
-
-                    let meals = documents.compactMap { doc -> MealEntry? in
-                        guard let foodName = doc["foodName"] as? String,
-                              let dateString = doc["date"] as? String,
-                              let date = ISO8601DateFormatter().date(from: dateString),
-                              let calories = doc["calories"] as? Int,
-                              let carbs = doc["carbs"] as? Int,
-                              let protein = doc["protein"] as? Int,
-                              let fats = doc["fats"] as? Int,
-                              let mealType = doc["mealType"] as? String
-                        else { return nil }
-
-                        return MealEntry(date: date, foodName: foodName, calories: calories, carbs: carbs, protein: protein, fats: fats, isManualEntry: true, mealType: mealType)
-                    }
-                    completion(meals)
-                } else {
-                    completion(nil)
-                }
-            } catch {
-                completion(nil)
             }
         }.resume()
     }
