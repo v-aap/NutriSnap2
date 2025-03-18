6f8e0b6d34020275e86c8b611c4d59bcdfc7dda3
Valeria Arce <145574961+v-aap@users.noreply.github.com>
2025-03-18 01:50:13 -0400

    Update NutriSnap2App.swift

--- NutriSnap2/NutriSnap2/NutriSnap2App.swift
+++ NutriSnap2/NutriSnap2/NutriSnap2App.swift
@@ -7,29 +7,45 @@
 
 import SwiftUI
 import FirebaseCore
-
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-
     return true
   }
 }
 
 @main
 struct NutriSnap2App: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @State private var isUserLoggedIn: Bool = Auth.auth().currentUser != nil
+    @State private var showSplash = true  // Controls splash screen visibility
 
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-            SplashScreenView()
-
-      }
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                if showSplash {
+                    SplashScreenView()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) { 
+                                showSplash = false
+                            }
+                        }
+                } else {
+                    if isUserLoggedIn {
+                        DashboardView()  // Show Dashboard if logged in
+                    } else {
+                        SignInView()  // Show Sign In screen if not logged in
+                    }
+                }
+            }
+            .onAppear {
+                Auth.auth().addStateDidChangeListener { _, user in
+                    isUserLoggedIn = (user != nil)
+                }
+            }
+        }
     }
-  }
 }
